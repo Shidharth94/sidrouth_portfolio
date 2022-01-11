d4708f042866077b198b370c2626b28d0ebb4983
Shidharth Routh <sidrouth@gmail.com>
2022-01-11 22:44:42 +0530

    php

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+
+    $message_sent = false;
+    if(isset($_POST['email']) && $_POST['email'] != ' ') {
+
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $userName = $_POST['name'];
+            $userEmail = $_POST['email'];
+            $userSubject = $_POST['subject'];
+            $userMessage = $_POST['message'];
+
+            $to = "sidrouth@gmail.com";
+            $body = "";
+
+            $body .= "From: ".$userName. "\r\n"; 
+            $body .= "E-mail: ".$userEmail. "\r\n"; 
+            $body .= "Subject: ".$userSubject. "\r\n"; 
+            $body .= "Message: ".$userMessage. "\r\n"; 
+
+            mail($to, $userSubject, $body);
+
+            $message_sent = true;
+        }
+        else{
+            $invalid_class_name = "form-invalid";
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -222,30 +253,40 @@
         </section>
         <div class="spacer layer4">
         </div>
+
+        <?php
+        if($message_sent):
+        ?>
+
+            <h3> Thanks, I'll be in touch</h3>
+
+        <?php
+        else:
+        ?>
         <section class="col5" id="sec5">
             <h3>Feel free to contact me !!</h3>
             <div class="container">
                 <div class="contact-left">
                     <h3>Send a Mail</h3>
-                    <form class="my-form" action="https://formsubmit.co/sidrouth@gmail.com" method="POST">
+                    <form class="my-form" action="index.php" method="POST">
                         <div class="details">
                             <div class="name s1">
                                 <label>Name : </label>
-                                <input type="text" placeholder="John Doe">
+                                <input type="text" placeholder="John Doe" name="name" class="form-control">
                             </div>
                             <div class="email s1">
                                 <label>E-mail : </label>
-                                <input type="email" placeholder="example@domain.com">
+                                <input type="email" placeholder="example@domain.com" name="email" class="form-control <?= $invalid_class_name ?? ""?> ">
                             </div>
                         </div>
                         <div class="content">
                             <div class="subject s1">
                                 <label>Subject : </label>
-                                <input type="text" placeholder="subject">
+                                <input type="text" placeholder="subject" name="subject" class="form-control">
                             </div>
                             <div class="message">
                                 <label>Message Content :</label>
-                                <textarea rows="5" placeholder="what do you want to talk about ?"></textarea>
+                                <textarea rows="5" placeholder="what do you want to talk about ?" name="message"></textarea>
                             </div>
                         </div>
                         <button type="submit"> Send Message</button>
@@ -266,6 +307,9 @@
                 </div>
             </div>
         </section>
+        <?php
+        endif;
+        ?>
     </main>
     <script src="main.js"></script>
 </body>
